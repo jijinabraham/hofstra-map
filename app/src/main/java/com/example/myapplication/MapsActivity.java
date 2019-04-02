8c8b86f81064a14804756bc408d6dc18e85dc72d
h-sd <fchen10@pride.hofstra.edu>
2019-04-02 10:34:44 -0400

    ui

--- app/src/main/java/com/example/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/myapplication/MapsActivity.java
@@ -1,350 +1,47 @@
 package com.example.myapplication;
 
-import android.Manifest;
-import android.app.Dialog;
-import android.content.pm.PackageManager;
-import android.content.res.Resources;
-import android.graphics.Color;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationListener;
-import android.os.Build;
-import android.provider.Telephony;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.api.Api;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.LocationSource;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.Circle;
-import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MapStyleOptions;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Polygon;
-import com.google.android.gms.maps.model.PolygonOptions;
-import com.google.android.gms.maps.model.Polyline;
-import com.google.android.gms.maps.model.PolylineOptions;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
-
-    static final int POLYGON_POINTS = 5;
-    GoogleMap mGoogleMap;
-    GoogleApiClient mGoogleApiClient;
-    Marker marker;
-    Circle circle;
-    Marker marker1;
-    Marker marker2;
-    Polyline line;
-    ArrayList<Marker> markers = new ArrayList<Marker>();
-    Polygon shape;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (googleServicesAvailable()) {
-            Toast.makeText(this, "Perfect!!!", Toast.LENGTH_LONG).show();
-            setContentView(R.layout.activity_maps);
-            initMap();
-        } else {
-            // no google maps layout
-        }
-    }
-
-    private void initMap() {
-        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-    public boolean googleServicesAvailable() {
-        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
-        int isAvailable = api.isGooglePlayServicesAvailable(this);
-        if (isAvailable == ConnectionResult.SUCCESS) {
-            return true;
-        } else if (api.isUserResolvableError(isAvailable)) {
-            Dialog dialog = api.getErrorDialog(this, isAvailable, 0);
-            dialog.show();
-        } else {
-            Toast.makeText(this, "Cant connect to play services", Toast.LENGTH_LONG).show();
 
-        }
-        return false;
-    }
-
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mGoogleMap = googleMap;
-        try {
-            boolean success = googleMap.setMapStyle(
-                    MapStyleOptions.loadRawResourceStyle(
-                            this, R.raw.mapstyle));
+        mMap = googleMap;
 
-            if (!success) {
-                Log.e("MapsActivity", "Style parsing failed.");
-            }
-        } catch (Resources.NotFoundException e) {
-            Log.e("MapsActivity", "Cant find style: Error", e);
-
-
-        }
-
-
-        if(mGoogleMap != null) {
-
-            mGoogleMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
-                @Override
-                public void onMarkerDragStart(Marker marker) {
-
-                }
-
-                @Override
-                public void onMarkerDrag(Marker marker) {
-
-                }
-
-                @Override
-                public void onMarkerDragEnd(Marker marker) {
-
-                    Geocoder gc = new Geocoder(MapsActivity.this);
-                    LatLng ll = marker.getPosition();
-                    double lat = ll.latitude;
-                    double lng = ll.latitude;
-                    List<Address> list = null;
-                    try {
-                         list = gc.getFromLocation(lat, lng, 1);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                    Address add = list.get(0);
-                    marker.setTitle(add.getLocality());
-                    marker.showInfoWindow();
-
-                }
-            });
-
-
-
-            mGoogleMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
-
-                @Override
-                public View getInfoWindow(Marker marker) {
-                    return null;
-                }
-
-                @Override
-                public View getInfoContents(Marker marker) {
-                    View v = getLayoutInflater().inflate(R.layout.info_window, null);
-
-                    TextView tvLocality = (TextView) v.findViewById(R.id.tv_locality);
-                    TextView tvLat = (TextView) v.findViewById(R.id.tv_lat);
-                    TextView tvLng = (TextView) v.findViewById(R.id.tv_lng);
-                    TextView tvSnippet = (TextView) v.findViewById(R.id.tv_snippet);
-
-                    LatLng ll = marker.getPosition();
-                    tvLocality.setText(marker.getTitle());
-                    tvLat.setText("Latitude: " + ll.latitude);
-                    tvLng.setText("Longitude: " + ll.longitude);
-                    tvSnippet.setText(marker.getSnippet());
-
-
-                    return v;
-                }
-            });
-
-        }
-        goToLocationZoom(40.71370920426692, -73.60166322969508, 15);
-
-
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
-
-    private void goToLocation(double lat, double lng) {
-        LatLng ll = new LatLng(lat, lng);
-        CameraUpdate update = CameraUpdateFactory.newLatLng(ll);
-        mGoogleMap.moveCamera(update);
-    }
-
-    private void goToLocationZoom(double lat, double lng, float zoom) {
-        LatLng ll = new LatLng(lat, lng);
-        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, zoom);
-        mGoogleMap.moveCamera(update);
-    }
-
-    public void geoLocate(View view) throws IOException {
-
-        EditText et = (EditText) findViewById(R.id.editText);
-        String location = et.getText().toString();
-
-        Geocoder gc = new Geocoder(this);
-        List<Address> list = gc.getFromLocationName(location, 1);
-        Address address = list.get(0);
-        String locality = address.getLocality();
-
-        Toast.makeText(this, locality, Toast.LENGTH_LONG).show();
-
-        double lat = address.getLatitude();
-        double lng = address.getLongitude();
-        goToLocationZoom(lat, lng, 15);
-
-        setMarker(locality, lat, lng);
-
-
-    }
-
-    private void setMarker(String locality, double lat, double lng) {
-       if (marker != null) {
-            removeEvertything();
-        }
-
-        if (markers.size() == POLYGON_POINTS) {
-            removeEverything();
-        }
-
-
-        MarkerOptions options = new MarkerOptions()
-                .title(locality)
-                .draggable(true)
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET))
-                .position(new LatLng(lat, lng))
-                .snippet("I am Here");
-
-        markers.add(mGoogleMap.addMarker(options));
-
-        if (markers.size() == POLYGON_POINTS) {
-            drawPolygon();
-        }
-
-        if(marker1 == null) {
-            marker1 = mGoogleMap.addMarker(options);
-        } else if(marker2 == null) {
-            marker2 = mGoogleMap.addMarker(options);
-            drawLine();
-        } else {
-            removeEvertything();
-            marker1 = mGoogleMap.addMarker(options);
-        }
-       circle  = drawCircle(new LatLng(lat, lng));
-
-
-    }
-
-    private void drawPolygon() {
-        PolygonOptions options = new PolygonOptions()
-                .fillColor(0x330000FF)
-                .strokeWidth(3)
-                .strokeColor(Color.RED);
-
-        for(int i=0; i<POLYGON_POINTS; i++) {
-            options.add(markers.get(i).getPosition());
-        }
-        shape = mGoogleMap.addPolygon(options);
-    }
-
-    private void removeEverything() {
-        for (Marker marker : markers) {
-            marker.remove();
-        }
-        markers.clear();
-        shape.remove();
-        shape = null;
-    }
-
-      private void drawLine() {
-
-        PolylineOptions options = new PolylineOptions()
-                .add(marker1.getPosition())
-                .add(marker2.getPosition())
-                .color(Color.BLUE)
-                .width(3);
-
-        line = mGoogleMap.addPolyline(options);
-    }
-
-     private Circle drawCircle(LatLng latLng) {
-
-        CircleOptions options = new CircleOptions()
-                .center(latLng)
-                .radius(1000)
-                .fillColor(0x33FF0000)
-                .strokeColor(Color.BLUE)
-                .strokeWidth(3);
-
-        return mGoogleMap.addCircle(options);
-    }
-
-   private void removeEvertything() {
-        marker1.remove();
-        marker1 = null;
-        marker2.remove();
-        marker2 = null;
-        line.remove();
-        circle.remove();
-        circle = null;
-
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.mapTypeNone:
-                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
-                break;
-            case R.id.mapTypeNormal:
-                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-                break;
-            case R.id.mapTypeSatellite:
-                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-                break;
-            case R.id.mapTypeTerrain:
-                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-                break;
-            case R.id.mapTypeHybrid:
-                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-                break;
-
-            default:
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 }
-
-
-
-
-
