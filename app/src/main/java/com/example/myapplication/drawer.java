ea70e76b9e346f8bbdb778971cb87cd8176a9a45
Xiangpu Conrad Chen <xchen48@pride.hofstra.edu>
2019-04-04 15:17:31 -0400

    Work in progress for drawer

--- app/src/main/java/com/example/myapplication/drawer.java
+++ app/src/main/java/com/example/myapplication/drawer.java
@@ -6,7 +6,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
 
 public class drawer extends AppCompatActivity {
 
