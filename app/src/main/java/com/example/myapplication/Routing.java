10f905da4bc3bf56ae60aeb3f8c11a0597cc4ecc
h-sd <fchen10@pride.hofstra.edu>
2019-04-27 23:54:18 -0400

    mod5-jijin

---
+++ app/src/main/java/com/example/myapplication/Routing.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+class Routing {
+    public void execute() {
+    }
+
+    public static class Builder {
+        public Object travelMode() {
+            return null;
+        }
+    }
+}
