1868c5400dc884cd07c757922d1bede9c9282548
h-sd <fchen10@pride.hofstra.edu>
2019-04-28 15:49:13 -0400

    mod5-jijin

--- app/src/main/java/com/example/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/myapplication/MapsActivity.java
@@ -1,22 +1,13 @@
 package com.example.myapplication;
 
-import android.Manifest;
 import android.app.Dialog;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.graphics.Color;
-import android.graphics.Point;
 import android.icu.text.Collator;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationListener;
-import android.net.RouteInfo;
-import android.os.Build;
-import android.provider.Telephony;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -30,20 +21,16 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.api.Api;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -53,7 +40,6 @@ import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.io.IOException;
-import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -108,17 +94,18 @@ public class MapsActivity<withListener> extends AppCompatActivity implements OnM
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(final GoogleMap googleMap) {
+
 
         Polyline polyline1 = googleMap.addPolyline(new PolylineOptions()
                 .clickable(true)
                 .add(
-                        new LatLng(40.71370920426692, -73.60166322969508),
-                        new LatLng(40.713995864573015, -73.60184025549006),
-                        new LatLng(40.71434570477824, -73.60163622399637),
-                        new LatLng(40.71463493439309, -73.60081230528436),
-                        new LatLng(40.71473860101402, -73.60048835734995),
-                        new LatLng(40.714940403322736, -73.59998685452882)));
+                        new LatLng(40.71370920426692, -73.60166322969508), //Memorial Hall,Bits & Bytes,Student Financial Services
+                        new LatLng(40.713995864573015, -73.60184025549006),  //Academic Advisement
+                        new LatLng(40.71434570477824, -73.60163622399637),   // Hauser Hall, Department of Psychology
+                        new LatLng(40.71463493439309, -73.60081230528436), // Axinn Library, The President's Office (West Wing), Honors College, bulletin Office
+                        new LatLng(40.71473860101402, -73.60048835734995),  // Hammer Computer Lab
+                        new LatLng(40.714940403322736, -73.59998685452882)));  // Heger Hall, Department of Religion, Department of Philosophy
 
         googleMap.setOnPolylineClickListener(this);
         googleMap.setOnPolygonClickListener(this);
@@ -126,17 +113,70 @@ public class MapsActivity<withListener> extends AppCompatActivity implements OnM
         Polyline polyline2 = googleMap.addPolyline(new PolylineOptions()
                 .clickable(true)
                 .add(
-                        new LatLng(0.71458813136285, -73.59937920078767),
-                        new LatLng(40.714275044106174, -73.60008193954957),
-                        new LatLng(40.71404307257277, -73.6003861264611),
-                        new LatLng(40.71397598202105, -73.60093329710014),
-                        new LatLng(40.7132828837781, -73.6013925075531),
-                        new LatLng(40.71354935646173, -73.60067386175535)));
+                        new LatLng(40.71385270564209, -73.59981633684794), // Brower Hall
+                        new LatLng(40.713004302992495, -73.60071195850378), // Adams Playhouse, The Globe Theater
+                        new LatLng(40.71418308199756, -73.59929521756487),  // Spiegel Theater
+                        new LatLng(40.71397598202105, -73.59997342215638),  // Calkins Hall, Department of Fine Arts, Department of Comparative Literature, department of Foreign Language
+                        new LatLng(40.712898632588455, -73.60015332698822),   //Dunkin' On The Quad
+                        new LatLng(40.71359843570906, -73.59923332929611)));  // Weller Hall, Business School
 
         googleMap.setOnPolylineClickListener(this);
         googleMap.setOnPolygonClickListener(this);
 
+
+
         polyline1.setTag("A");
+
+        Polyline polyline3 = googleMap.addPolyline(new PolylineOptions()
+                .clickable(true)
+                .add(
+                        new LatLng(40.71458813136285, -73.59937920078767),  //Mason Hall, Gallon-Wing, Department of English, Department of Women's Studies, Department of Writing Studies
+                        new LatLng(40.714275044106174, -73.60008193954957), //Philips Hall, Administrative Offices
+                        new LatLng(40.71404307257277, -73.6003861264611),   // Hofstra Hall, Development Office
+                        new LatLng(40.71397598202105, -73.60093329710014),  // Davison Hall, Department of Anthropology, Department of Sociology
+                        new LatLng(40.7132828837781, -73.6013925075531),   //Emily Lowe Hall, Emily Lowe Gallery, Department of Drama & Dance
+                        new LatLng(40.71354935646173, -73.60067386175535)));   //Barnard Hall, Department of Economics, Department of Political Science
+
+        googleMap.setOnPolylineClickListener(this);
+        googleMap.setOnPolygonClickListener(this);
+
+        Polyline polyline4 = googleMap.addPolyline(new PolylineOptions()
+                .clickable(true)
+                .add(
+                        new LatLng(40.71308813580013, -73.59921991825104), //McEwen Hall, Department of Information Technology, Department of Telecommunications, Help Desk
+                        new LatLng(40.71291024148623, -73.5990500642539), //Au Bon Pain
+                        new LatLng(40.71258310439361, -73.59954073283473), //Roosevelt Hall, Department of Mathematics, Departmentof Global Studies and Geography,International Student Affairs
+                        new LatLng(40.712530244017586, -73.60069140050211), //The Lawrence Herbert School of Communication, Media School
+                        new LatLng(40.71223439623034, -73.59996385130682), //Bunny & Charles A. Koppelman Hall, Maurice A. Deane School of Law, Law School
+                        new LatLng(40.7119700925464, -73.59949446472922))); //Kushner Hall, School of Law Library, Maurice A. Deane School of Law, Law School
+
+        googleMap.setOnPolylineClickListener(this);
+        googleMap.setOnPolygonClickListener(this);
+
+        Polyline polyline5 = googleMap.addPolyline(new PolylineOptions()
+                .clickable(true)
+                .add(
+                        new LatLng(40.71193857934487, -73.60060573976449),  //Breslin Hall
+                        new LatLng(40.71330040059968, -73.60242175490225),  //Adams Hall, Department of Computer Science
+                        new LatLng(40.71310929186988, -73.60296624333228),  //Weed Hall, Department of Engineering
+                        new LatLng(40.7137850231927, -73.60264344390424),  //Bernon Hall, Admission Center
+                        new LatLng(40.71368946959187, -73.60424204047712),  //Saltzman Community Services Center, Community Services Center, Hofstra University Continuing Education
+                        new LatLng(40.71454548110428, -73.601107348276)));  //West Library Wing
+
+        googleMap.setOnPolylineClickListener(this);
+        googleMap.setOnPolygonClickListener(this);
+
+        Polyline polyline6 = googleMap.addPolyline(new PolylineOptions()
+                .clickable(true)
+                .add(
+                        new LatLng(40.71517207788174, -73.60098022454923),   //unispan
+                        new LatLng(40.714141333060816, -73.60393283345525),  // netherspan
+                        new LatLng(40.71290305111857, -73.60209711604841)));  //Joan & Donald Schaeffer Black Box Theater
+
+        googleMap.setOnPolylineClickListener(this);
+        googleMap.setOnPolygonClickListener(this);
+
+
 
         mGoogleMap = googleMap;
         try {
@@ -264,7 +304,6 @@ public class MapsActivity<withListener> extends AppCompatActivity implements OnM
 
         MarkerOptions options = new MarkerOptions()
                 .title("Adams Hall")
-                .title("Academic Advisement")
                 .draggable(true)
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET))
                 .position(new LatLng(lat, lng))
@@ -417,14 +456,16 @@ public class MapsActivity<withListener> extends AppCompatActivity implements OnM
         private String mLatitude;
         private String mLongtitude;
         private String mBuilding;
-        private String mHouseNumber;
         private String mDepartment;
-        private String mCity;
 
         private Location mMarkerLocation;
 
-        public GeoLocation(Context context) throws IOException {
+        public GeoLocation(Context context, String mLatitude, String mLongtitude, String mBuilding, String mDepartment) throws IOException {
             mContext = context;
+            this.mLatitude = mLatitude;
+            this.mLongtitude = mLongtitude;
+            this.mBuilding = mBuilding;
+            this.mDepartment = mDepartment;
         }
 
         public String getBuilding() {
@@ -449,10 +490,10 @@ public class MapsActivity<withListener> extends AppCompatActivity implements OnM
         private double latitude;
         List<Address> addresses  = geocoder.getFromLocation(latitude,longitude, 1);
 
-        String city = addresses.get(0).getSubLocality();
-        String state = addresses.get(0).getAdminArea();
-        String zip = addresses.get(0).getPostalCode();
-        String country = addresses.get(0).getSubAdminArea();
+        String Building = addresses.get(0).getSubLocality();
+        String Department = addresses.get(0).getAdminArea();
+        String Latitude = addresses.get(0).getPostalCode();
+        String Longitude = addresses.get(0).getSubAdminArea();
     }
 }
 
